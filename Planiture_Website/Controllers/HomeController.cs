1c290d8c5a30b8b732f7acf80c3e4f7edad223b9
Chantell Babootie <babootiechantell@gmail.com>
2020-06-03 21:13:07 -0400

    Add project files.

---
+++ Planiture_Website/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Planiture_Website.Models;
+
+namespace Planiture_Website.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Menu()
+        {
+            return View();
+        }
+
+        public IActionResult Investments()
+        {
+            return View();
+        }
+
+        public IActionResult Subscriptions()
+        {
+            return View();
+        }
+
+        public IActionResult PlanitureAcademy()
+        {
+            return View();
+        }
+
+        public IActionResult Security()
+        {
+            return View();
+        }
+
+        public IActionResult CustomerService()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
